0f823753b1381f2b4494b2ae7d6661f5c9918977
stephenmwise <162519976+stephenmwise@users.noreply.github.com>
2024-09-26 18:31:16 -0400

    Added initial scraper functionality

---
+++ main.py
@@ -0,0 +1,44 @@
+import requests
+import selectorlib
+import os
+import time
+
+URL = "https://www.scrapethissite.com/pages/simple/"
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+
+def scrape(url):
+    '''Scrape the page source from the URL'''
+    response = requests.get(url, headers=HEADERS)
+    source = response.text
+    return source
+
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["countries"]
+    return value
+
+def provide_info():
+    print("Info is sent!")
+
+def store(extracted):
+    with open("data.txt", "a") as file:
+        file.write(extracted + "\n")
+
+def read(extracted):
+    with open("data.txt", "r") as file:
+        return file.read()
+
+if __name__ == "__main__":
+    while True:
+        scraped = scrape(URL)
+        extracted = extract(scraped)
+        print(extracted)
+
+        content = read(extracted)
+        if extracted:
+            if extracted not in "data.txt":
+                store(extracted)
+                provide_info()
+            time.sleep(2)
